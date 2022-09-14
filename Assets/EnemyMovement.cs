3d41121c985f20f9d723239df9ece8761e652a09
Peter Nedergaard <peter@nedergaard.org>
2022-09-14 11:01:22 +0200

    classExercise scene

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    private NavMeshAgent navMeshAgent;
+    public Transform Player;
+    private float aggroRange = 15f; 
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    
+    void Update()
+    {
+        navMeshAgent.SetDestination(Player.position);
+        
+        if (Vector3.Distance(transform.position, Player.position) < aggroRange)
+        {
+            navMeshAgent.isStopped = false; 
+
+            navMeshAgent.SetDestination(Player.position);
+        }
+        else
+        {
+            navMeshAgent.isStopped = true;
+        }
+    }
+}
