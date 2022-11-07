01cb3cff61b69ca9ccbacf72d2fdb6a2bc1d42dd
Peter Nedergaard <peter@nedergaard.org>
2022-11-07 21:30:15 +0100

    Create GameHandler, new-round UI

--- Assets/StartScreenScript.cs
+++ Assets/StartScreenScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,22 +9,37 @@ public class StartScreenScript : MonoBehaviour
     public GameObject buttonCard;
     public Transform startCam;
     public GameObject player;
+    public GameObject gameHandler;
     private Transform mainCam;
-    private GameObject _canvas;
-    private bool gameStarted;
+    private GameObject canvas;
+    private bool translateCam;
 
     private float smoothTime = 10f;
     private Vector3 velocityPos;
     private Vector3 velocityRot;
+    private Vector3 targetPos;
+    private Vector3 targetRot;
+
+    private TextMeshProUGUI title;
+    private float colorChange;
+
+    private float colorMin = 0.4f;
+    private float colorMax = 0.8f;
+    private float t;
     
-    
+
     void Awake()
     {
         Button startBtn = buttonCard.transform.Find("StartButton").GetComponent<Button>();
         Button quitBtn = buttonCard.transform.Find("QuitButton").GetComponent<Button>();
-        _canvas = transform.Find("Canvas").gameObject;
-        mainCam = player.transform.Find("Main Camera");
         
+        canvas = transform.Find("Canvas").gameObject;
+        mainCam = player.transform.Find("Main Camera");
+        title = canvas.transform.Find("Title").GetComponent<TextMeshProUGUI>();
+        
+        targetPos = mainCam.position;
+        targetRot = mainCam.eulerAngles;
+
         startBtn.onClick.AddListener(StartGame);
         quitBtn.onClick.AddListener(QuitGame);
     }
@@ -31,38 +47,83 @@ public class StartScreenScript : MonoBehaviour
 
     void Update()
     {
-        if (gameStarted)
+        if (translateCam)
         {
-            Vector3 targetPos = mainCam.position;
-            Vector3 targetRot = mainCam.eulerAngles;
+            TranslateToPlayerCam();
+        }
+        else
+        {
+            PulseTitleColor();
+        }
+    }
+    
 
-            if (startCam.eulerAngles.y > 180) targetRot.y += 360;
 
-            startCam.position = Vector3.SmoothDamp(startCam.position, targetPos, ref velocityPos, smoothTime * Time.deltaTime);
-            startCam.eulerAngles = Vector3.SmoothDamp(startCam.eulerAngles, targetRot, ref velocityRot, smoothTime * Time.deltaTime);
+    private void TranslateToPlayerCam()
+    {
+        startCam.position = Vector3.SmoothDamp(startCam.position, targetPos, ref velocityPos, smoothTime * Time.deltaTime);
+        startCam.eulerAngles = Vector3.SmoothDamp(startCam.eulerAngles, targetRot, ref velocityRot, smoothTime * Time.deltaTime);
 
-            float posDiff = startCam.position.sqrMagnitude - mainCam.position.sqrMagnitude;
-            if (posDiff < 0) posDiff *= -1;
+        if (IsCamAtTarget())
+        {
+            ToggleGameActors();
+            translateCam = false;
+        }
+    }
+    
+    
+    private void ToggleGameActors()
+    {
+        player.SetActive(!player.activeSelf);
+        gameHandler.SetActive(!gameHandler.activeSelf);
+        gameHandler.GetComponent<GameHandler>().AnnounceNextRound();
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+    
 
-            if (posDiff < 1)
-            {
-                player.SetActive(true);
-                transform.gameObject.SetActive(false);
-            }
+    private bool IsCamAtTarget()
+    {
+        bool result = false;
+        float posDiff = startCam.position.sqrMagnitude - mainCam.position.sqrMagnitude;
+        
+        if (posDiff < 0) posDiff *= -1;
+
+        if (posDiff < 1)
+        {
+            result = true;
+        }
+
+        return result;
+    }
+
+    
+    private void PulseTitleColor()
+    {
+        Color color = new Color(Mathf.Lerp(colorMin, colorMax, t), title.color.g, title.color.b);
+        title.color = color;
+
+        t += 0.5f * Time.deltaTime;
+
+        if (t >= 1f)
+        {
+            // colorMax and colorMin values gets switched. Alternative to using a temporary variable
+            (colorMax, colorMin) = (colorMin, colorMax);
+            t = 0;
         }
     }
 
-
+    
     void StartGame()
     {
-        gameStarted = true;
-        _canvas.SetActive(false);
+        translateCam = true;
+        canvas.SetActive(false);
         transform.Find("CameraPivot").GetComponent<Animation>().enabled = false;
+        
+        if (startCam.eulerAngles.y > 180) targetRot.y += 360;
     }
 
     void QuitGame()
     {
         Application.Quit();
-        // UnityEditor.EditorApplication.isPlaying = false;
     }
 }
